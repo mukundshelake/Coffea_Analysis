803daf51db643b9ba81345e50b82d0ecec5a4aef
Mukund Shelake <mukundshelake400@gmail.com>
2023-08-25 16:19:17 +0530

    Moved helper funcs to lib/helpers

---
+++ Scripts/lib/helpers.py
@@ -0,0 +1,25 @@
+import glob, os
+def getfileset(eraDir):
+	fileset = {}
+	for setName in glob.glob(os.path.join(eraDir,'*/*')):
+		if 'mu' in setName:
+			continue
+		lst = []
+		for file in glob.glob(os.path.join(setName,'*/*/*/*/*.root')):
+			print(file)
+			if os.path.isfile(file):
+				if 'mu' in file:
+					pass
+				lst.append(file)
+		setName = setName.split('/')[-1]
+		fileset[setName] = lst
+	return fileset
+
+
+def getFiles(folder_path):
+    file_list = []
+    for file_name in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, file_name)):
+            file_list.append(os.path.join(folder_path, file_name))
+    return file_list
+
