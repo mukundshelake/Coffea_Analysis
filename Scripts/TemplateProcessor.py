0dbe4cd40aec5e663cbffab0e9cbaed977ae38f8
Mukund Shelake <mukundshelake400@gmail.com>
2023-09-20 01:36:25 +0200

    Separated 'el' and 'mu' input files

--- Scripts/TemplateProcessor.py
+++ Scripts/TemplateProcessor.py
@@ -122,12 +122,15 @@ class NanoProcessor(processor.ProcessorABC):
 
    
 
-inputDir = '/nfs/home/common/RUN2_UL/Tree_crab/SIXTEEN_preVFP'
-# If you are using getFiles from the lib.helpers 
-#fileset = {
-#    "Data": getFiles('/home/mukund2/Projects/CoffeaTrial/Files/Data'),
-#    "MC": getFiles('/home/mukund2/Projects/CoffeaTrial/Files/MC')
-#}
+DataDir = '/nfs/home/common/RUN2_UL/Tree_crab/SIXTEEN_preVFP/Data_el'
+MCDir = '/nfs/home/common/RUN2_UL/Tree_crab/SIXTEEN_preVFP/MC'
+
+inputDir = [DataDir, MCDir]
+# # If you are using getFiles from the lib.helpers 
+# fileset = {
+#    "Data": getfileset('/nfs/home/common/RUN2_UL/Tree_crab/SIXTEEN_preVFP/Data_el/'),
+#    "MC": getfileset('/nfs/home/common/RUN2_UL/Tree_crab/SIXTEEN_preVFP/MC/')
+# }
 
 
 # Generate the fileset in the appropriate format for the input directory.
--- Scripts/lib/helpers.py
+++ Scripts/lib/helpers.py
@@ -1,14 +1,16 @@
 import glob, os
-def getfileset(eraDir):
+def getfileset(DirList):
 	fileset = {}
-	for setName in glob.glob(os.path.join(eraDir,'*/*')):
-		lst = []
-		for file in glob.glob(os.path.join(setName,'*/*/*/*/*.root')):
-			print(file)
-			if os.path.isfile(file):
-				lst.append(file)
-		setName = setName.split('/')[-1]
-		fileset[setName] = lst
+	for eraDir in DirList:
+		for setName in glob.glob(os.path.join(eraDir,'*')):
+			lst = []
+			print(setName)
+			for file in glob.glob(os.path.join(setName,'*/*/*/*/*.root')):
+				#print(file)
+				if os.path.isfile(file):
+					lst.append(file)
+			setName = setName.split('/')[-1]
+			fileset[setName] = lst
 	return fileset
 
 
