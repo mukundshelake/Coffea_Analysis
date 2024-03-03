ccda69007bd4fb10900cd2d23c98f515cd779ae3
Mukund Shelake <mukundshelake400@gmail.com>
2024-03-03 16:07:59 +0530

    Removed junk

--- Scripts/trial.py
+++
@@ -1,7 +0,0 @@
-import glob, os, json
-from lib.helpers import getFiles
-
-
-with open('Datasets/sampleFiles_UL2016preVFP.json', 'r') as json_file:
-    fileset = json.load(json_file) 
-print({**fileset['Data_el'], **fileset['MC_el']})
