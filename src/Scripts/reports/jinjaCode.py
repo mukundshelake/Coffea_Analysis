9c88bbe9dc6bb328bfc1d31c2a8e657856ebb3c4
Mukund Shelake <mukundshelake400@gmail.com>
2024-04-25 19:22:26 +0530

    First Reports

--- src/Scripts/reports/jinjaCode.py
+++ src/Scripts/reports/jinjaCode.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 
 # Set up the environment
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('trial.tex')
+template = env.get_template('unfilled.tex')
 
 # Data to fill in the template
 data = {
@@ -14,5 +14,5 @@ data = {
 output = template.render(data)
 
 # Save the rendered LaTeX code
-with open('output.tex', 'w') as f:
+with open('filled.tex', 'w') as f:
     f.write(output)
