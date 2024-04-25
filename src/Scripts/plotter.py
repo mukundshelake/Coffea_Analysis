77baa723a97c5393196b54e4eb1b07f716721396
Mukund Shelake <mukundshelake400@gmail.com>
2024-04-25 18:06:05 +0530

    added parameter study scripts

--- src/Scripts/plotter.py
+++ src/Scripts/plotter.py
@@ -1,18 +1,65 @@
-from coffea.util import load
+from coffea.util import load, save
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-
-out = load("coffeaOutput.coffea")
-ytProjection = out["DoubleMuon"]["DoubleMuon"]["yMatrix"].project("y_t")
-ytbarProjection = out["UL2016preVFP_ttbarSemi_Sample"]["UL2016preVFP_ttbarSemi_Sample"]["yMatrix"].project("y_tbar")
-
-with PdfPages('output_plots.pdf') as pdf:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ytProjection.plot(ax=ax)
-    ytbarProjection.plot(ax=ax)
-    # ax.set_xscale("log")
-    ax.legend(title="Dimuon charge")
-    pdf.savefig() 
+import pandas as pd
+import os
 
 
+outputDir = 'outputs'
+coffeaFile = "Fq_results.coffea"
+
+
+# out = load("Output.coffea")
+out = load(os.path.join(outputDir,coffeaFile))
+
+plotDictionary = {
+    "Fu Distribution": {'xAxis': 'y0', 'yAxis' : 'Fu'},
+    "Fd Distribution": {'xAxis': 'y0', 'yAxis' : 'Fd'},
+    "Fu_ud Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Fu_ud'},
+    "Fd_ud Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Fd_ud'},
+    "Du Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Dd'},
+    "Fu vs Fd Distribution" : {'xAxis' : 'Fu', 'yAxis' : 'Fd'},
+    "Fu_ud vs Fd_ud Distribution" : {'xAxis' : 'Fu_ud', 'yAxis' : 'Fd_ud'},
+    "Du vs Dd Distribution" : {'xAxis' : 'Du', 'yAxis' : 'Dd'},
+    "FuDu Distribution" : {'xAxis' : 'y0', 'yAxis' : 'FuDu'},
+    "FdDd Distribution" : {'xAxis' : 'y0', 'yAxis' : 'FdDd'},
+    "FuDu vs FdDd Distribution" : {'xAxis' : 'FuDu', 'yAxis' : 'FdDd'},
+}
+
+regions = ['A', 'B', 'C', 'D']
+
+# Create the main "figures" directory if it doesn't exist
+figures_folder = os.path.join("plots", "paramDistributions")
+os.makedirs(figures_folder, exist_ok=True)
+
+for era in out:
+    era_folder = os.path.join(figures_folder, era)
+    os.makedirs(era_folder, exist_ok=True)  # Create era sub-directory
+    results = out[era]['Y0_variations']
+    for region in regions:
+        region_folder = os.path.join(era_folder, f"region_{region}")
+        os.makedirs(region_folder, exist_ok=True)  # Create region sub-sub-directory
+        
+        for plot in plotDictionary:
+            xAxis = plotDictionary[plot]['xAxis']
+            yAxis = plotDictionary[plot]['yAxis']
+            x_array = np.zeros(24)
+            y_array = np.zeros(24)
+            for index, (key, value) in enumerate(results.items()):
+                x_array[index] = key
+                if xAxis != 'y0':
+                    x_array[index] = value[region][xAxis]
+                y_array[index] = value[region][yAxis]
+            plt.figure()
+            plt.title(f"{plot} ({region}) in {era}")
+            plt.plot(x_array, y_array)
+            plt.xlabel(xAxis)
+            plt.ylabel(yAxis)
+            
+            # Save the figure in the appropriate sub-folder
+            plt.savefig(os.path.join(region_folder, f"{yAxis}_vs_{xAxis}.png"))
+            plt.close()
+
+print("Figures saved in separate folders based on era and regions.")
 
