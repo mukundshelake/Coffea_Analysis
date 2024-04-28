d23b0f763c4350072ecdb4f410c5eaa96f013be1
Mukund Shelake <mukundshelake400@gmail.com>
2024-04-29 02:19:54 +0530

    completed reports

--- src/Scripts/plotter.py
+++ src/Scripts/plotter.py
@@ -1,18 +1,68 @@
 from coffea.util import load
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+import pandas as pd
+import os
 
-out = load("coffeaOutput.coffea")
-ytProjection = out["DoubleMuon"]["DoubleMuon"]["yMatrix"].project("y_t")
-ytbarProjection = out["UL2016preVFP_ttbarSemi_Sample"]["UL2016preVFP_ttbarSemi_Sample"]["yMatrix"].project("y_tbar")
+outputDir = 'outputs'
+coffeaFile = "Fq_results.coffea"
 
-with PdfPages('output_plots.pdf') as pdf:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ytProjection.plot(ax=ax)
-    ytbarProjection.plot(ax=ax)
-    # ax.set_xscale("log")
-    ax.legend(title="Dimuon charge")
-    pdf.savefig() 
+
+# out = load("Output.coffea")
+out = load(os.path.join(outputDir,coffeaFile))
+
+plotDictionary = {
+    "Fu Distribution": {'xAxis': 'y0', 'yAxis' : 'Fu', 'figName' : 'Fu'},
+    "Fd Distribution": {'xAxis': 'y0', 'yAxis' : 'Fd', 'figName' : 'Fd'},
+    "Fu_ud Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Fu_ud', 'figName' : 'Fu_ud'},
+    "Fd_ud Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Fd_ud', 'figName' : 'Fd_ud'},
+    "Du Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Du', 'figName' : 'Du'},
+    "Dd Distribution" : {'xAxis' : 'y0', 'yAxis' : 'Dd', 'figName' : 'Dd'},
+    "Fu vs Fd Distribution" : {'xAxis' : 'Fu', 'yAxis' : 'Fd', 'figName' : 'Fu_vs_Fd'},
+    "Fu_ud vs Fd_ud Distribution" : {'xAxis' : 'Fu_ud', 'yAxis' : 'Fd_ud', 'figName' : 'Fu_ud_vs_Fd_ud'},
+    "Du vs Dd Distribution" : {'xAxis' : 'Du', 'yAxis' : 'Dd' , 'figName' : 'Du_vs_Dd'},
+    "FuDu Distribution" : {'xAxis' : 'y0', 'yAxis' : 'FuDu', 'figName' : 'FuDu'},
+    "FdDd Distribution" : {'xAxis' : 'y0', 'yAxis' : 'FdDd', 'figName' : 'FdDd'},
+    "FuDu vs FdDd Distribution" : {'xAxis' : 'FuDu', 'yAxis' : 'FdDd', 'figName' : 'FuDu_vs_FdDd'},
+}
+
+regions = ['A', 'B', 'C', 'D']
+
+# Create the main "figures" directory if it doesn't exist
+figures_folder = os.path.join("plots", "paramDistributions")
+os.makedirs(figures_folder, exist_ok=True)
+
+for era in out:
+    era_folder = os.path.join(figures_folder, era)
+    os.makedirs(era_folder, exist_ok=True)  # Create era sub-directory
+    results = out[era]['Y0_variations']
+    for region in regions:
+        region_folder = os.path.join(era_folder, f"region_{region}")
+        os.makedirs(region_folder, exist_ok=True)  # Create region sub-sub-directory
+        
+        for plot in plotDictionary:
+            xAxis = plotDictionary[plot]['xAxis']
+            yAxis = plotDictionary[plot]['yAxis']
+            x_array = np.zeros(24)
+            y_array = np.zeros(24)
+            for index, (key, value) in enumerate(results.items()):
+                x_array[index] = key
+                if xAxis != 'y0':
+                    x_array[index] = value[region][xAxis]
+                y_array[index] = value[region][yAxis]
+            plt.figure()
+            plt.title(f"{plot} ({region}) in {era}")
+            plt.plot(x_array, y_array)
+            plt.xlabel(xAxis)
+            plt.ylabel(yAxis)
+            
+            # Save the figure in the appropriate sub-folder
+            plt.savefig(os.path.join(region_folder, f"{plotDictionary[plot]['figName']}.png"))
+            plt.close()
+
+print("Figures saved in separate folders based on era and regions.")
+
 
 
 
--- src/Scripts/reports/jinjaCode.py
+++ src/Scripts/reports/jinjaCode.py
@@ -1,14 +1,29 @@
 from jinja2 import Environment, FileSystemLoader
+from coffea.util import load, save
+# Configure Jinja with custom block start and end strings
+env = Environment(loader=FileSystemLoader('.'), variable_start_string='((*', variable_end_string='*))', block_start_string='((%', block_end_string='%))', comment_start_string='<!--', comment_end_string='-->')
 
-# Set up the environment
-env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('unfilled.tex')
 
 # Data to fill in the template
-data = {
-    'name': 'John Doe',
-    'age': 30
-}
+data = {}
+for era in ['UL2016preVFP', 'UL2016postVFP', 'UL2017', 'UL2018']:
+    for region in ['A', 'B', 'C', 'D']:
+        for param in ['Fu', 'Fd', 'Fu_ud', 'Fd_ud', 'Du', 'Dd', 'FuDu', 'FdDd', 'Fu_vs_Fd', 'Du_vs_Dd', 'Fu_ud_vs_Fd_ud', 'FuDu_vs_FdDd']:
+            keyName = f'{param}_{era}_{region}'
+            valueName = f'../plots/paramDistributions/{era}/region_{region}/{param}.png'
+            data[keyName] = valueName
+
+out = load('../outputs/Fq_results.coffea')
+for era in ['UL2016preVFP', 'UL2016postVFP', 'UL2017', 'UL2018']:
+    for param in ['Fu', 'Fd', 'Fu_ud', 'Fd_ud', 'Du', 'Dd', 'FuDu', 'FdDd']:
+        key_withFlow = f"{param}_{era}_withFlow"
+        value_withFlow = round(out[era]['Full_y0_range']['with_flow'][param],4)
+        data[key_withFlow] = value_withFlow
+        key_withoutFlow = f"{param}_{era}_withoutFlow"
+        value_withoutFlow = round(out[era]['Full_y0_range']['without_flow'][param],4)
+        data[key_withoutFlow] = value_withoutFlow
+
 
 # Render the template with data
 output = template.render(data)
