a622d93126c90dc81c107a7a00c9fd07b053edf1
Mukund Shelake <mukundshelake400@gmail.com>
2024-07-25 04:36:39 +0530

    Automated commit before running script

--- src/Scripts/Fitting_AFB/workflow.sh
+++ src/Scripts/Fitting_AFB/workflow.sh
@@ -90,7 +90,7 @@ commit_hash=$(get_git_commit)
 log "Current Git commit hash: $commit_hash"  # No timestamp for this message
 
 # Store the commands in variables
-skimmer_command="python skimmer.py $skimmer_args -o $output_file -c ttbar_SemiLeptonic"
+skimmer_command="python skimmer.py $skimmer_args -o $output_file -c ttbar_SemiLeptonic -e UL2016preVFP"
 message_bot_command="python messegeBOT.py"
 
 # Log and execute the commands
